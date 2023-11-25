18c8341de5f59c0dae401f71c216c8e575187555
cristopherkovalski <cristopherkovalski@gmail.com>
2023-11-25 01:41:17 -0300

    rabbit list

---
+++ demo/src/main/java/gerentews/com/bantads/service/RabbitService.java
@@ -0,0 +1,42 @@
+package gerentews.com.bantads.service;
+
+import java.security.NoSuchAlgorithmException;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+import gerentews.com.bantads.model.IdMensagemDTO;
+
+@Service
+public class RabbitService {
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    GerenteService gerService;
+
+    
+    @RabbitListener(queues = "saga-gerente-deletegerente-init")
+    public void receiveMessageSaga(@Payload IdMensagemDTO message) throws NoSuchAlgorithmException {
+        try {
+            Long id = message.getId();
+            gerService.deletarGerente(id);
+            IdMensagemDTO mensagem = new IdMensagemDTO();
+            mensagem.setId(id);
+            mensagem.setMessage("gerente deletado do gerenteMS");
+            rabbitTemplate.convertAndSend("saga-gerente-deletegerente-end", mensagem);
+
+        } catch (Exception e) {
+             IdMensagemDTO mensagem = new IdMensagemDTO();
+             mensagem.setErro(true);
+             mensagem.setMessage("erro ao deletar gerente: " + e);
+             rabbitTemplate.convertAndSend("saga-gerente-deletegerente-end", mensagem);
+            System.out.println("erro " + e);
+        }
+
+    }
+
+}
